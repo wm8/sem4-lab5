e2538f416312889c416b77ea0257284592d00d75
BuildTools <unconfigured@null.spigotmc.org>
2022-02-09 10:09:50 +0300

    build v1.1

--- include/tests.hpp
+++ include/tests.hpp
@@ -1,8 +1,8 @@
-// Copyright 2021 Your Name <your_email>
+// Copyright 2022 wm8
 
-#ifndef INCLUDE_EXAMPLE_HPP_
-#define INCLUDE_EXAMPLE_HPP_
+#ifndef INCLUDE_TEST_HPP_
+#define INCLUDE_TEST_HPP_
 
 #include "Stack.h"
 
-#endif // INCLUDE_EXAMPLE_HPP_
+#endif // INCLUDE_TEST_HPP_
